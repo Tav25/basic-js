29916124ad8c1f9346afeffa911c5ed47ea23ad9
Turbin Alex <turbin28@gmail.com>
2020-09-30 23:20:06 +0300

    feat: count-cat, dream team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  // console.log(matrix.length)
+  let cat = 0;
+  for (x in matrix){
+    // console.log(matrix[x])
+    for (i in matrix[x]){ 
+      // console.log(matrix[x][i])
+      if ( matrix[x][i] === '^^') {cat++;}
+    }
+  }
+  return cat;
+  };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(arrName) {
+  if (Array.isArray(arrName) === false) { return false } // if not array
+  let nameDreamTeam = [];
+  for (x in arrName) {
+    let name = arrName[x]
+    if (typeof name === 'string') { // test string and uppercase if 173 all lowercase
+      nameDreamTeam.push(name.trim()[0].toUpperCase()); // toUpperCase for all lowercase array
+      nameDreamTeam.sort().join(''); //sort 
+    }
+  }
+  return nameDreamTeam.join('');
+}
