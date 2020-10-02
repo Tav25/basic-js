028821915ec1e8a73930ed0d2b94ad534c60871d
Tav25 <turbin28@gmail.com>
2020-10-03 00:16:44 +0300

    feat: transform array

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,6 +3,6 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function calculateHanoi(disc, speed) {
   const steps = 2 ** disc - 1;// 2**n-1
   const secMin = Math.floor(steps / (speed / 3600));// 
-  console.log(`    ${steps} ${secMin}`);
+  // console.log(`    ${steps} ${secMin}`);
   return { turns: steps, seconds: secMin };
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
+console.log("-------------------------------------------------------------------------------------------")
+module.exports = function transform(arr) {
+  console.log("**********")
+  if (Array.isArray(arr) === false) { throw new Error('Error! no Array!') }
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  // console.log(arr)
+  let newArr = [];
+  for (x in arr) {
+    // console.log(x)
+    if (typeof arr[x] === "string") {
+      // console.log("len: " +arr.length)
+      if (arr[x] === "--discard-next") {newArr.splice((x * 1) + 1, 1); }
+      else if (arr[x] === "--discard-prev" ){ newArr.splice((x * 1) - 1, 1);}
+      else if (arr[x] === "--double-next" &&  x !== (arr.length-1).toString() ){ newArr.push(arr[(x * 1) + 1]); }
+      else if(arr[x] === "--double-prev" &&  x !== "0" ){ newArr.push(arr[(x * 1) - 1]); }
+    }
+    else {
+      newArr.push(arr[x])
+    }
+  }
+  console.log(` ${arr} @@@@@ ${newArr} `)//
+  return newArr;
 };
