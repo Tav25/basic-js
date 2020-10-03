49e0c42e4b8641aa64005b82e5165b9396ad1901
Turbin Alex <turbin28@gmail.com>
2020-10-03 13:11:40 +0300

    fix: transform array (1failing)

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,24 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 console.log("-------------------------------------------------------------------------------------------")
-module.exports = function transform(arr) {
-  console.log("**********")
-  if (Array.isArray(arr) === false) { throw new Error('Error! no Array!') }
+module.exports = function transform(arrIndex) {
+  // console.log(`[${arr}]`);
+  if (Array.isArray(arrIndex) === false) { throw new Error('Error! no Array!') }
+  
+  let newArr = [];
+  let arr = [];
+  let command = ["--discard-next", "--discard-prev", "--double-next", "--double-prev"]
 
   // console.log(arr)
-  let newArr = [];
+  for (x in arrIndex){ arr.push(arrIndex[x])}
+
   for (x in arr) {
     // console.log(x)
-    if (typeof arr[x] === "string") {
+    // console.log(command.includes(arr[x]) + "**************");
+    if (command.includes(arr[x])) {
       // console.log("len: " +arr.length)
-      if (arr[x] === "--discard-next") {newArr.splice((x * 1) + 1, 1); }
-      else if (arr[x] === "--discard-prev" ){ newArr.splice((x * 1) - 1, 1);}
-      else if (arr[x] === "--double-next" &&  x !== (arr.length-1).toString() ){ newArr.push(arr[(x * 1) + 1]); }
-      else if(arr[x] === "--double-prev" &&  x !== "0" ){ newArr.push(arr[(x * 1) - 1]); }
+      if (arr[x] === "--discard-next" || arr[x] === "--discard-next") {
+        arr.splice((x * 1), 1);
+        arr.splice((x * 1) + 1, 1);
+        if (arr[x * 1 + 2] === "--double-prev") { console.log("dop") }
+      }
+      else if (arr[x] === "--discard-prev") { newArr.splice((x * 1) - 1, 1); }
+      else if (arr[x] === "--double-next" && x !== (arr.length - 1).toString()) { newArr.push(arr[(x * 1) + 1]); }
+      else if (arr[x] === "--double-prev" && x !== "0") { newArr.push(arr[(x * 1) - 1]); }
     }
     else {
       newArr.push(arr[x])
     }
   }
-  console.log(` ${arr} @@@@@ ${newArr} `)//
+  console.log(`-----------------------------
+in: [${arrIndex}]
+res:[${newArr}]
+
+`)//
   return newArr;
 };
